7a3b022570615863fc77f90b7b2828d74027e57a
lgpy <lgpy17@gmail.com>
2026-03-18 11:29:05 +0000

    improvement

--- apps/bot/entrypoint.sh
+++ apps/bot/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-
-# migrate
 echo "Running migrations..."
 bun run --cwd packages/db db:migrate
 
 echo "Starting bot..."
-bun run turbo bot:start
+bun run --cwd apps/bot bot:start
